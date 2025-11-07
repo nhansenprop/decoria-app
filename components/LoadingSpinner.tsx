212cda5fbbcb70698ff0ad6adf464ee7b9f684fe
nhansenprop <nhansenprop@gmail.com>
2025-11-06 23:31:55 -0300

    Update LoadingSpinner.tsx

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -1,9 +1,8 @@
-
 import React from 'react';
 
 const LoadingSpinner: React.FC = () => {
   return (
-    <div className="border-4 border-gray-200 border-t-[#FF5100] rounded-full w-12 h-12 animate-spin"></div>
+    <div className="border-4 border-gray-200 border-t-brand-orange rounded-full w-12 h-12 animate-spin"></div>
   );
 };
 
